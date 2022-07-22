161e72b1f3eb189c753b68bcbb462230e6e04b36
Nazar-Sergeev <privat.nazar@gmail.com>
2022-07-22 11:46:14 +0300

    lesson 1

---
+++ lesson1/main.js
@@ -0,0 +1,72 @@
+// let a = 'hello';
+// let b = 'owu.';
+// let c = 'com.';
+// let d = 'ua';
+// console.log(`${a} ${b}${c}${d}`);
+// alert(a);
+// alert(`${b}${c}${d}`);
+// document.write(`${a} ${b}${c}${d}`);
+
+// let n1 = 1;
+// console.log(n1);
+// alert(n1);
+// document.write(n1);
+
+// let n2 = 10;
+// console.log(n2);
+// alert(n2);
+// document.write(n2);
+
+// let n3 = -999;
+// console.log(n3);
+// alert(n3);
+// document.write(n3);
+
+// let n4 = 123;
+// console.log(n4);
+// alert(n4);
+// document.write(n4);
+
+// let pi = 3.14;
+// console.log(pi);
+// alert(pi);
+// document.write(pi);
+
+// let n6 = 2.7;
+// console.log(n6);
+// alert(n6);
+// document.write(n6);
+
+// let bool1 = true;
+// console.log(bool1);
+// alert(bool1);
+// document.write(bool1);
+
+// let bool2 = false;
+// console.log(bool2);
+// alert(bool2);
+// document.write(bool2);
+
+// let firstName = 'Nazar';
+// let middleName = 'Yuriyovych';
+// let lastName = 'Serheiev';
+//
+// let person = `${lastName} ${firstName} ${middleName}`;
+// console.log(person);
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+// let name = prompt('What`s your name?');
+// console.log(name);
+//
+// let middleName = prompt('What`s your middle name?');
+// console.log(middleName);
+//
+// let age = prompt('How old are you?');
+// console.log(age);
