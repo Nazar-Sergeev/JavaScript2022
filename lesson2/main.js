6cca99a7c81ae15b8ece2d10bf8accf02ba9d628
Nazar-Sergeev <privat.nazar@gmail.com>
2022-07-27 16:07:48 +0300

    lesson 2

---
+++ lesson2/main.js
@@ -0,0 +1,136 @@
+
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+//
+// const array =['hello okten', 145, 587, true, false, 'kokos', 'abrikos', 999, 854, 963];
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+// console.log(array[3]);
+// console.log(array[4]);
+// console.log(array[5]);
+// console.log(array[6]);
+// console.log(array[7]);
+// console.log(array[8]);
+// console.log(array[9]);
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+//
+// const book1 = {
+//     title: 'Garry Potter',
+//     pageCount: 1000,
+//     genre: 'Fantastic'
+// };
+//
+// const book2 = {
+//     title: 'Eloquent',
+//     pageCount: 500,
+//     genre: 'programming'
+// };
+//
+// const book3 = {
+//     title: 'JavaScript Minifier',
+//     pageCount: 400,
+//     genre: 'programming'
+// };
+
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив.Кожен автор має поля name,age
+//
+// const book1 = {
+//     title: 'Garry Potter',
+//     pageCount: 1000,
+//     genre: 'Fantastic',
+//     authors: [
+//         {name: 'Vasya', age: 50},
+//         {name: 'Kolya', age: 63}
+//     ],
+//
+// };
+// console.log(book1);
+//
+// const book2 = {
+//     title: 'Eloquent',
+//     pageCount: 500,
+//     genre: 'programming',
+//     authors: [
+//         {name: 'Kiril', age: 30},
+//         {name: 'Volodya', age: 28}
+//     ]
+// };
+// console.log(book2);
+//
+// const book3 = {
+//     title: 'JavaScript Minifier',
+//     pageCount: 400,
+//     genre: 'programming',
+//     authors:[
+//         {name: 'Kostya', age: 37},
+//         {name: 'Nikita', age: 33}
+//     ]
+// };
+// console.log(book3);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password. Вивести в консоль пароль кожного користувача
+//
+// const users = [
+//     {
+//         name: 'Vasya',
+//         userName: 'Molotok',
+//         password: 'hello123'
+//     },
+//     {
+//         name: 'Kolya',
+//         userName: 'Brama',
+//         password: 'brama123'
+//     },
+//     {
+//         name: 'Max',
+//         userName: 'Dudnik',
+//         password: 'dudnik123'
+//     },
+//     {
+//         name: 'Mark',
+//         userName: 'Troyan',
+//         password: 'troyan123'
+//     },
+//     {
+//         name: 'Nazar',
+//         userName: 'Rumba',
+//         password: 'rumba123'
+//     },
+//     {
+//         name: 'Sveta',
+//         userName: 'Zaremba',
+//         password: 'zaremba123'
+//     },
+//     {
+//         name: 'Tanya',
+//         userName: 'Kuvalda',
+//         password: 'kuvalda123'
+//     },
+//     {
+//         name: 'Oleg',
+//         userName: 'Vorun',
+//         password: 'vorun123'
+//     },
+//     {
+//         name: 'John',
+//         userName: 'Yaruk',
+//         password: 'yaruk123'
+//     },
+//     {
+//         name: 'Jek',
+//         userName: 'Wik',
+//         password: 'wik123'
+//     },
+// ]
+// console.log(`${users[0].name} - password: ${users[0].password}`);
+// console.log(`${users[1].name} - password: ${users[1].password}`);
+// console.log(`${users[2].name} - password: ${users[2].password}`);
+// console.log(`${users[3].name} - password: ${users[3].password}`);
+// console.log(`${users[4].name} - password: ${users[4].password}`);
+// console.log(`${users[5].name} - password: ${users[5].password}`);
+// console.log(`${users[6].name} - password: ${users[6].password}`);
+// console.log(`${users[7].name} - password: ${users[7].password}`);
+// console.log(`${users[8].name} - password: ${users[8].password}`);
+// console.log(`${users[9].name} - password: ${users[9].password}`);
